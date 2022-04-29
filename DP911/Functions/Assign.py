7b59fce6934c1ac249937c31548b6710be84c5fa
beibao233 <beibao2333@gmail.com>
2022-04-29 17:36:49 +0800

    Add more functions

---
+++ DP911/Functions/Assign.py
@@ -0,0 +1,10 @@
+from DP911.Functions import describe
+from DP911.Registers.Variables import Variable
+
+
+"""1C NN MM"""
+introduce = describe("1C", 1, 1)
+
+
+def main(save_address: str or int, read_address: str or int):
+    save_address(save_address, Variable.read(read_address))
---
+++ DP911/Functions/GreaterEqualJump.py
@@ -0,0 +1,15 @@
+from DP911.Tools import ishex, set_range
+from DP911.Functions import describe
+from DP911.Registers.Variables import Variable
+
+
+"""1A NN,NN MM,MM"""
+introduce = describe("1A", 2, 2)
+
+
+def main(com_number: str or int, address: str or int):
+    set_range(0x0, ishex(com_number), 0xFFFF)
+    set_range(0x0, ishex(address), 0xFFFF)
+
+    if ishex(com_number) >= Variable.read(Variable.temp_register):
+        return ishex(address)
