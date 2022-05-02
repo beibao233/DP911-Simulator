de2ff20a50bb0f62a111dd93c8089da3946bd91e
beibao233 <beibao2333@gmail.com>
2022-05-02 16:50:34 +0800

    Web change

--- web.py
+++ web.py
@@ -1,35 +1,3 @@
-from flask import Flask, request, render_template, send_from_directory
-from DP911.Registers import Code
-from DP911.Interpreter import run
-from gevent import pywsgi
-import sys
-import os
+from Web.web import start_server
 
-app = Flask(__name__)
-
-
-@app.route('/logo.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'Resource'),
-                               'logo.ico', mimetype='image/vnd.microsoft.icon')
-
-
-@app.route("/", methods=['GET', 'POST'])
-async def editor():
-    if request.method == 'POST':
-        try:
-            return render_template("result.html",
-                                   results_msg=run(Code.sim_storage_space(request.form['code']), web_mode=True),
-                                   address=Code.sim_storage_space(request.form['code']))
-        except Exception:
-            return render_template("result.html",
-                                   data=sys.exc_info(),
-                                   address=Code.sim_storage_space(request.form['code']),
-                                   mode=0)
-    else:
-        return render_template("editor.html")
-
-
-if __name__ == '__main__':
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
-    server.serve_forever()
+start_server()
