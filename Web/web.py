96a952d92d3491d39deb98c1c524532117145cea
beibao233 <beibao2333@gmail.com>
2022-05-02 19:01:59 +0800

    code style stay

--- Web/web.py
+++ Web/web.py
@@ -17,19 +17,26 @@ def favicon():
 @app.route("/", methods=['GET', 'POST'])
 async def editor():
     if request.method == 'POST':
+        code = request.form['code'].replace('\n', '%br')
+
         try:
             return render_template("result.html",
-                                   code=request.form['code'],
+                                   code=code,
                                    results_msg=run(Code.sim_storage_space(request.form['code']), web_mode=True),
                                    address=Code.sim_storage_space(request.form['code']))
         except Exception:
             return render_template("result.html",
-                                   code=request.form['code'],
+                                   code=code,
                                    data=sys.exc_info(),
                                    address=Code.sim_storage_space(request.form['code']),
                                    mode=0)
     else:
-        return render_template("editor.html", code=request.args.get("code"))
+        if str(request.args.get("code")) == "None":
+            code = ""
+        else:
+            code = request.args.get("code").replace('%br', '\n')
+
+        return render_template("editor.html", code=code)
 
 
 def start_server(ip='0.0.0.0', port=5000):
